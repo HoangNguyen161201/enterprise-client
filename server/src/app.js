f5f3638abd713b604f29cc1eda1cd587153d3c46
nguyen quang hoang <hoanghpang@gmail.com>
2022-02-08 16:11:13 +0700

    update gitignore

--- server/src/app.js
+++ server/src/app.js
@@ -7,6 +7,10 @@ app.get('/', (req, res)=> {
     res.send('nguyen quang hoang')
 })
 
+app.get('/hello', (req, res)=> {
+    res.send('hello')
+})
+
 app.listen(PORT, ()=> {
     console.log(`http://localhost:${PORT}`)
 })
