c45a473eb2c39efd2d7df8ebc0e2ffc97784d023
huy <huynguyencc1000@gmail.com>
2022-02-08 21:22:58 +0700

    Test update file to git

---
+++ components/elements/buttonPri.tsx
@@ -0,0 +1,15 @@
+import { Button } from 'antd';
+import * as React from 'react';
+
+export interface IbuttonPriProps {
+}
+
+export default function buttonPri (props: IbuttonPriProps) {
+  return (
+    <div>
+      <Button type="primary">
+          Nguyen Quang Huy
+      </Button>
+    </div>
+  );
+}
