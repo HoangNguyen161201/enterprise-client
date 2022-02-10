47d7ad0fda1d602f3322b842ab62b7a7a3d5c22b
huy <huynguyencc1000@gmail.com>
2022-02-10 16:02:00 +0700

    Config prettierrc

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,32 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import {ConfigProvider} from 'antd'
-import { useEffect } from 'react'
-import Head from 'next/head'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ConfigProvider } from 'antd';
+import { useEffect } from 'react';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-
   // custom color button
-  useEffect(()=> {
+  useEffect(() => {
     ConfigProvider.config({
       theme: {
         primaryColor: '#009F9D',
-        infoColor: 'black'
+        infoColor: 'black',
       },
-    })
-  },[])
+    });
+  }, []);
   return (
     <>
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com"/>
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600&display=swap" rel="stylesheet"/>
-        </Head>
-        <Component {...pageProps} />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
     </>
-  ) 
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,18 @@
-import {NextPage} from 'next'
-import { Button } from 'antd'
-import 'antd/dist/antd.variable.min.css'
+import { NextPage } from 'next';
+import { Button } from 'antd';
+import 'antd/dist/antd.variable.min.css';
 
-
-const index:NextPage = ()=> {
-
+const index: NextPage = () => {
   return (
     <div>
-      <Button type='primary' id={'button'}>Enter with me</Button>
-      <Button type='default' id={'button'}>Enter with me</Button>
+      <Button type="primary" id={'button'}>
+        Enter with me
+      </Button>
+      <Button type="default" id={'button'}>
+        Enter with me
+      </Button>
     </div>
-    
-  )
-}
+  );
+};
 
-export default index
+export default index;
