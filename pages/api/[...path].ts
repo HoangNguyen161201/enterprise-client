28e099c4ae11e4eabf350105536fe13f6d5d549f
huy <huynguyencc1000@gmail.com>
2022-02-11 15:31:20 +0700

    Create proxy web by nextjs

---
+++ pages/api/[...path].ts
@@ -0,0 +1,40 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import httpProxy from 'http-proxy';
+
+//Config body parser let pass to server api by proxy server
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+//Create server proxy
+const proxy = httpProxy.createProxyServer();
+
+export default function handle(req: NextApiRequest, res: NextApiResponse<any>) {
+  return new Promise((resolve) => {
+    //Don't send cookies to API server
+    req.headers.cookie = '';
+
+    //Proxy server send req and res to target server
+    // /api/students => http://localhost:4000/api/students
+    proxy.web(req, res, {
+      target: process.env.API_URL,
+      changeOrigin: true,
+      selfHandleResponse: false,
+    });
+
+    //Because self handle response is false, don't need response
+    // res.status(200).json({
+    //     name: "PATH - Match all here"
+    // })        // res.status(200).json({
+    //     name: "PATH - Match all here"
+    // })
+
+    proxy.once('proxyRes', () => {
+      resolve(true);
+    });
+  });
+}
---
+++ pages/api/auth/logout.ts
@@ -0,0 +1,71 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import httpProxy, { ProxyResCallback } from 'http-proxy';
+import Cookies from 'cookies';
+
+//Config body parser let pass to server api by proxy server
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+//Create server proxy
+const proxy = httpProxy.createProxyServer();
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+  //Check method
+  if (req.method !== 'POST') {
+    return res.status(400).json({
+      err: 'Method not valid.',
+    });
+  }
+
+  //Return promise
+  return new Promise((resolve) => {
+    const cookies = new Cookies(req, res);
+
+    //Don't send cookies to API server
+    req.headers.cookie = '';
+
+    //Proxy server send req and res to target server
+    // /api/students => http://localhost:3000/api/students
+    proxy.web(req, res, {
+      target: process.env.API_URL,
+      changeOrigin: true,
+      selfHandleResponse: true,
+    });
+
+    const handleLogoutResponse: ProxyResCallback = (proxyRes, req, res) => {
+      let body = '';
+      proxyRes.on('data', function (chunk) {
+        body += chunk;
+      });
+
+      proxyRes.on('end', function () {
+        try {
+          const { status, msg, err, statusCode } = JSON.parse(body);
+
+          //Remove access and refresh token cookie
+          if (status) {
+            cookies.set('refresh_token');
+          }
+
+          ;(res as NextApiResponse).status(statusCode).json({
+            status,
+            msg,
+            err,
+          });
+        } catch (error) {
+          ;(res as NextApiResponse).status(500).json({
+            err: 'Something went wrong.',
+          });
+        }
+
+        resolve(true);
+      });
+    };
+
+    proxy.once('proxyRes', handleLogoutResponse);
+  });
+}
