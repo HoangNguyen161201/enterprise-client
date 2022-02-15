2f5a51ed394a97a68a02d5defd154c3af49b425b
nguyen quang hoang <hoanghpang@gmail.com>
2022-02-15 13:49:26 +0700

    update serverSideProps

---
+++ pages/table.tsx
@@ -0,0 +1,17 @@
+import React, { useMemo } from 'react';
+import Table from '../components/elements/Table';
+import { ClientLayout } from '../components/layouts';
+import { NextPageWithLayout } from '../models';
+
+const table: NextPageWithLayout = () => {
+
+    return (
+    <div>
+      <Table />
+    </div>
+  );
+};
+
+table.getLayout = ClientLayout;
+
+export default table;
