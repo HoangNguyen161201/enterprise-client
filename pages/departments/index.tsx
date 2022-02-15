357a15976c8332640ea685f6a4911a814494cd75
nguyen quang hoang <hoanghpang@gmail.com>
2022-02-15 15:02:59 +0700

    front-end table

--- pages/departments/index.tsx
+++ pages/departments/index.tsx
@@ -1,16 +1,95 @@
-import { Breadcrumb, Card, Space } from 'antd';
+import { Breadcrumb, Card, Space, Table, Tag } from 'antd';
 import * as React from 'react';
 import { ClientLayout } from '../../components/layouts';
 import { NextPageWithLayout } from '../../models/layoutType';
-import {useState} from 'react'
+import { useState } from 'react';
 import { GetServerSideProps } from 'next';
+import { ColumnsType } from 'antd/lib/table';
+import { DeleteOutlined, EditOutlined, ProfileOutlined, UsergroupAddOutlined } from '@ant-design/icons';
 
 export interface IAddDepartmentProps {}
 
 const AddDepartment: NextPageWithLayout = (props: IAddDepartmentProps) => {
-  const [data, setData] = useState([
+  const dataSource = [
+    {
+      key: 1,
+      name: 'nguyen quang hoang',
+      root: true,
+      staff: 24,
+      detail: 'dfd',
+      update: 'dfdf',
+      remove: 'dfdfd',
+      assign: 'dfs'
+    },
+    {
+      key: 2,
+      name: 'nguyen quang hoang',
+      root: false,
+      staff: 24,
+      detail: 'dfd',
+      update: 'dfdf',
+      remove: 'dfdfd',
+      assign: 'dfs'
+    }
+  ]
+  
+  const columns:ColumnsType = [
+    {
+      title: 'name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'root',
+      dataIndex: 'root',
+      key: 'root',
+      render: (value: boolean)=> (
+        <>
+          {
+            value == true ? <Tag color="green">true</Tag>: <Tag color="red">false</Tag>
+          }
+        </>
+      )
+    },
+    {
+      title: 'staff',
+      dataIndex: 'staff',
+      key: 'staff',
+    },
+    {
+      title: 'detail',
+      dataIndex: 'detail',
+      key: 'detail',
+      render: (value)=> (
+        <ProfileOutlined style={{color: '#07456F'}}/>
+      )
+    },
+    {
+      title: 'Assign',
+      dataIndex: 'assign',
+      key: 'assign',
+      render: (value)=> (
+        <UsergroupAddOutlined style={{color: '#07456F'}}/>
+      )
+    },
+    {
+      title: 'Update',
+      dataIndex: 'update',
+      key: 'update',
+      render: (value)=> (
+        <EditOutlined style={{color: '#1890ff'}} />
+      )
+    },
+    {
+      title: 'Delete',
+      dataIndex: 'remove',
+      key: 'remove',
+      render: (value)=> (
+        <DeleteOutlined style={{color: 'red'}} />
+      )
+    },
+  ]
 
-  ]) 
   return (
     <>
       <Breadcrumb>
@@ -18,11 +97,9 @@ const AddDepartment: NextPageWithLayout = (props: IAddDepartmentProps) => {
         <Breadcrumb.Item>All Departments</Breadcrumb.Item>
       </Breadcrumb>
 
-      <Card
-        title="All Departments"
-        style={{ width: '100%', marginTop: '20px' }}
-      >
+      <Card title="All Departments" style={{ width: '100%', marginTop: '20px' }}>
         <Space direction="vertical" size={20}></Space>
+        <Table style={{overflowX: 'auto', }} dataSource={dataSource} columns={columns}/>
       </Card>
     </>
   );
@@ -30,23 +107,26 @@ const AddDepartment: NextPageWithLayout = (props: IAddDepartmentProps) => {
 
 export default AddDepartment;
 
-AddDepartment.getLayout = function getLayout({ children }) {
-  return <ClientLayout>{children}</ClientLayout>;
-};
+AddDepartment.getLayout = ClientLayout
 
-export const getServerSideProps: GetServerSideProps = async (context)=> {
-  console.log(context.req.headers.cookie)
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  console.log(context.req.headers.cookie);
   const res = await fetch('http://localhost:3000/api/auth/accesstoken', {
     method: 'GET',
     headers: {
       cookie: context.req.headers.cookie,
     } as HeadersInit,
   });
-  console.log(res)
 
+  const result = await res.json();
+  if (result.statusCode == 401)
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    };
   return {
-    props: {
-
-    }
+    props: {}
   }
-}
+};
