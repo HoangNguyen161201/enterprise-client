2bc1b1431a546d41b1e12a588e3cc40e41b16c55
huy <huynguyencc1000@gmail.com>
2022-02-16 17:37:57 +0700

    fetch user not department

--- pages/departments/assign/[id].tsx
+++ pages/departments/assign/[id].tsx
@@ -9,7 +9,7 @@ import { useForm } from 'react-hook-form';
 import { Input, Select, TextArea } from '../../../components/elements';
 import { ClientLayout } from '../../../components/layouts';
 import { NextPageWithLayout } from '../../../models';
-import { getCurrentUser, getDetailDepartment, getUsersRoleDepartment } from '../../../queries';
+import { getCurrentUser, getDetailDepartment, getUsersNotDepartment, getUsersRoleDepartment } from '../../../queries';
 import { validateAddDepartment } from '../../../utils';
 
 export interface IAssignDepartmentProps {}
@@ -35,8 +35,7 @@ const AssignDepartment: NextPageWithLayout = (props: IAssignDepartmentProps) => 
   );
 
     //Get list users not have department
-    const { error: errorUsersNotDPM, data: dataUsersnotDPM } = getDetailDepartment(
-      id as string,
+    const { error: errorUsersNotDPM, data: dataUsersnotDPM } = getUsersNotDepartment(
       dataUser?.accessToken.token
     );
 
