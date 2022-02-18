21ec5da521d14bcc058437b3ce721dbe450dc8e1
huy <huynguyencc1000@gmail.com>
2022-02-18 15:02:54 +0700

    Prepare fetch data for dev ui CRUD categories

---
+++ queries/category.ts
@@ -0,0 +1,20 @@
+import { AxiosError } from 'axios';
+import { useQuery } from 'react-query';
+import { IallCategories } from '../models/apiType';
+import { getData } from '../utils';
+
+export const getallCategories = (accessToken: string, initial?: IallCategories) => {
+  return useQuery<any, AxiosError>(
+    ['categories'],
+    async () => {
+      return await getData({ url: `/api/category`, token: accessToken });
+    },
+    {
+      enabled: !!accessToken,
+      retry: 1,
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      initialData: initial,
+    }
+  );
+};
