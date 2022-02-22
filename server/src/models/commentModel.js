22c707a2c7a608579aa80d9db34510e8aaaf9a99
huy <huynguyencc1000@gmail.com>
2022-02-22 15:46:51 +0700

    Fix define comment model

--- server/src/models/commentModel.js
+++ server/src/models/commentModel.js
@@ -1,26 +1,41 @@
 const mongoose = require('mongoose');
 
 //Define schema
-const commentSchema = new mongoose.Schema({
-  content: {
-    type: String,
-    required: true,
-  },
+const replySchema = new mongoose.Schema({
   user_id: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'users',
     required: true,
   },
-  idea_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'ideas',
-    required: true,
-  },
-  comment_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'comments',
-    default: null,
-  },
+
+  content: {
+      required: true,
+      type: String
+  }
 }, {timestamps: true});
+
+const commentSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'users',
+      required: true,
+    },
+    idea_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'ideas',
+      required: true,
+    },
+    replies: {
+      default: [],
+      type: [replySchema]
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('comments', commentSchema);
