e6d88afbd7ae58637a6a4345ff35bd05f7a1ac95
NgTienTrong <trongtiens96@gmail.com>
2022-02-24 14:43:39 +0700

    add user_id and idea_id into viewdb

--- server/src/routes/mainRouter.js
+++ server/src/routes/mainRouter.js
@@ -5,14 +5,11 @@ const departmentRouter = require('./departmentRouter');
 const submissionRouter = require('./submissionRouter');
 const categoryRouter = require('./categoryRouter');
 const viewRouter = require('./viewRouter');
-<<<<<<< HEAD
 const ideaRouter = require('./ideaRoute');
 
-=======
 const reactionRouter = require('./reactionRouter');
 const reactionTypeRouter = require('./reactionTypeRouter');
 const commentRouter = require('./commentRouter');
->>>>>>> 070befc47cce51d3c1cf25a93723a8e34a44d811
 
 //Config main router
 mainRouter = (app) => {
@@ -28,15 +25,13 @@ mainRouter = (app) => {
 
   app.use('/api/views', viewRouter);
 
-<<<<<<< HEAD
   app.use('/api/idea', ideaRouter);
-=======
+  
   app.use('/api/reactions', reactionRouter);
 
   app.use('/api/reactiontypes', reactionTypeRouter); 
 
   app.use('/api/comments', commentRouter); 
->>>>>>> 070befc47cce51d3c1cf25a93723a8e34a44d811
 };
 
 module.exports = mainRouter;
