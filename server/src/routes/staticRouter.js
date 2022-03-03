ac8ccf79f7113f4482b5c3678838995a4061e71d
nguyen quang hoang <hoanghpang@gmail.com>
2022-03-03 11:48:30 +0700

    api static

--- server/src/routes/staticRouter.js
+++ server/src/routes/staticRouter.js
@@ -10,5 +10,13 @@ const staticRouter = express.Router();
 const authorization = require('../middlewares/authorization');
 
 staticRouter.get('/status-ideas', staticController.statusIdeas);
+staticRouter.get('/many-idea', staticController.hasManyIdeas);
+staticRouter.get('/top-view', staticController.topView);
+staticRouter.get('/count', staticController.countAll);
+staticRouter.get('/percent-idea', staticController.percentIdea);
+staticRouter.get('/anonymously', staticController.anonymously);
+staticRouter.get('/accept', staticController.accept);
+staticRouter.get('/numberOPC', staticController.numberOPC);
+staticRouter.get('/idea-By-Date', staticController.ideaByDate);
 
 module.exports = staticRouter;
