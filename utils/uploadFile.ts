8e3c13ca64f1173dcff04a1f647b7f470146c632
nguyen quang hoang <hoanghpang@gmail.com>
2022-03-07 09:45:43 +0700

    update image

--- utils/uploadFile.ts
+++ utils/uploadFile.ts
@@ -1,4 +1,4 @@
-export const uploadFile = async (files: File[], tags: String[])=> {
+export const uploadFile = async (files: File[], tags: String[], avatar: false)=> {
     const data: any = []
     await Promise.all(
         files.map(async (file) => {
@@ -6,7 +6,11 @@ export const uploadFile = async (files: File[], tags: String[])=> {
             return new Promise(async (resolve) => {
                 const formData = new FormData()
                 formData.append('file', file)
-                formData.append('upload_preset', `${process.env.UPLOAD_PRESET}`)
+                if(avatar) {
+                    formData.append('upload_preset', `${process.env.UPLOAD_PRESET_AVATAR}`)
+                } else {
+                    formData.append('upload_preset', `${process.env.UPLOAD_PRESET}`)
+                }
                 formData.append('api_key',  `${process.env.API_KEY}`)
                 formData.append('cloud_name', `${process.env.CLOUD_NAME}`)
                 tags.map((tag: any )=> {
