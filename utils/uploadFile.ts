985c65b394342375e5f5b96e9fe850dc8977a932
nguyen quang hoang <hoanghpang@gmail.com>
2022-05-02 14:49:08 +0700

    update

--- utils/uploadFile.ts
+++ utils/uploadFile.ts
@@ -6,9 +6,6 @@ export const uploadFile = async (files: File[], tags: String[], avatar: boolean,
             return new Promise(async (resolve) => {
                 const formData = new FormData()
                 formData.append('file', file)
-                if(folder) {
-                    formData.append('folder',`hoang`)
-                } 
                 if(avatar) {
                     formData.append('upload_preset', `${process.env.UPLOAD_PRESET_AVATAR}`)
 
